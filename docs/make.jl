80fccc44d05b677f1a4ce3b18a43e15b042772e5
le-schwa <84086902+le-schwa@users.noreply.github.com>
2023-03-22 22:42:29 +0100

    Update sitename

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(;
     modules=[Dodds_Watts_model],
     authors="Leander Schwarzmeier <leander-schwarzmeier@web.de>",
     repo="https://github.com/le-schwa/Dodds_Watts_model.jl/blob/{commit}{path}#{line}",
-    sitename="Dodds_Watts_model.jl",
+    sitename="Dodds_Watts_model.jl Documentation",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         edit_link="main",
